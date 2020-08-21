453c10f827fcba91b9fb303fa115a3bb8174d5e5
WebProjectGroup01 <WebProjectGroup@i.ua>
2020-08-21 17:38:09 +0300

    Update javascript.js

--- Task1/JavaScript/javascript.js
+++ Task1/JavaScript/javascript.js
@@ -1,4 +1,4 @@
 var firstName = prompt('Какое ваше имя?');
 var lastName = prompt('Какая ваша фамилия?');
-var Age = prompt('Сколько вам лет?');
+var age = prompt('Сколько вам лет?');
 alert("Пользователь: "+ firstName +" " + lastName + "\nВозраст: "+ Age);
