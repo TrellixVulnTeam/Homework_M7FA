95ad6f24bd7e7b36a1eed5f8b761cf78cc19af8b
WebProjectGroup01 <WebProjectGroup@i.ua>
2020-08-27 23:41:12 +0300

    Update JS.js

--- Task5_guessColor/JavaScript/JS.js
+++ Task5_guessColor/JavaScript/JS.js
@@ -1,12 +1,11 @@
 const COLOR = "green";
 alert("Игра угадай цвет, на угадывание дается 3 попытки. Удачи)")
-for(var i = 2;i >= 0; --i){
+for (var i = 2; i >= 0; --i) {
     var variant = prompt("Введите цвет:")
-    if(variant == "green") {
+    if (variant == "green") {
         alert("Вы угадали");
         break;
-    }
-    else {
+    } else {
         alert("К сожелению у вас осталось " + i + " попыток")
     }
 }
