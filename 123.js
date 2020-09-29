1d088d29bd50dbde5d466b5fee2270fe5a992972
WebProjectGroup01 <WebProjectGroup@i.ua>
2020-09-30 00:56:16 +0300

    DOM

--- 123.js
+++ 123.js
@@ -1,20 +1,85 @@
-// примеры для деструкторизации 
-let user1 = {
-    name: 'John',
-    age: 20,
-    surname: 'Fishman',
+    // let listOfStudentsButton = document.querySelector('#list-of-students-button');
+    // listOfStudentsButton.onclick = function () {
+    //     let list = teacher.getListOfStudentsByAverageMark();
+    //     console.log(list);
+    // }
+
+window.onload = init;
+
+function init() {
+    let describe = document.querySelector('#describe');
+    let get = document.querySelector('#get');
+    console.log(describe, get);
+
+    describe.onclick = function () {
+
+        let fillOutForm = document.getElementById('fill-out-form');
+        let childForms = fillOutForm.children;
+        let radioValue = fillOutForm.elements.who.value;
+        
+        let color = childForms.color.value,
+            breed = childForms.breed.value,
+            name = childForms.name.value;
+            number = childForms.number.value;
+        
+        Animal.describeAnimal({
+            cat: +radioValue == 0,
+            dog: +radioValue == 1,
+            color,
+            breed,
+            name,
+            number: +number
+        });
+    }
+
 }
-let { name, ...all} = user1;
 
-let newO =  {...all, name: 'Name'};
+class Animal {
+    static shalter = [];
 
-// console.log(name, all);
+    constructor({
+        cat,
+        dog,
+        color,
+        breed,
+        name,
+        number
+    }) {
+            this.cat = cat,
+            this.dog = dog,
+            this.color = color,
+            this.breed = breed,
+            this.name = name,
+            this.number = number
+    }
 
-// let {name: n, age: a} = user1;
-// console.log(n, a);
+    static describeAnimal(data) {
+        Animal.setAnimal(new Animal(data))
+    }
 
+    static setAnimal(animal) {
+        console.log(animal);
+        Animal.shalter.push(animal);
+        console.log(Animal.shalter);
 
-// let arr = [1,2];
-// let [first, ...all] = arr;
-// console.log(first);
-let newarr = [...all,3,5,6]
+    }
+    static getAnimals(key, value) {
+        return shalter.filter(function (animal, index) {
+            animal[key] === value;
+        })
+    }
+
+    static getAnimal(key, value) {
+        return shalter.find(function (animal, index) {
+            animal[key] === value;
+        })
+    }
+
+    changeName(number, newName) {
+        let findedAnimal = Animal.getAnimal('number', number);
+        if(!findedAnimal) return;
+
+        findedAnimal.name = newName;
+    }
+
+}
--- Destructuring/JavaScript/JS.js
+++ Destructuring/JavaScript/JS.js
@@ -8,8 +8,8 @@ let info = {
 
 // С помощью деструктуризации создайте правильный объект.
 let {neme:name, surname:surname, specializatin:specialization} = info;
-
-info = {
+delete info;
+let info = {
   name: "Michael",
   surname: "Scofield",
   specialization: "Architect"
@@ -17,38 +17,38 @@ info = {
 
 //---------------2------------------
 
-const student = {
-    name: 'John Doe',
-    age: 16,
-    scores: {
-        maths: 74,
-        english: 63,
-        science: 85
-    }
-};
+// const student = {
+//     name: 'John Doe',
+//     age: 16,
+//     scores: {
+//         maths: 74,
+//         english: 63,
+//         science: 85
+//     }
+// };
 
-function showInfo({
-    name,
-    scores: {
-        maths = 0,
-        english = 0,
-        science = 0
-    }
-}) {
-    console.log('Hello, ' + name);
-    console.log('Your Maths score is ' + maths);
-    console.log('Your English score is ' + english);
-    console.log('Your Science score is ' + science);
-}
-showInfo(student);
+// function showInfo({
+//     name,
+//     scores: {
+//         maths = 0,
+//         english = 0,
+//         science = 0
+//     }
+// }) {
+//     console.log('Hello, ' + name);
+//     console.log('Your Maths score is ' + maths);
+//     console.log('Your English score is ' + english);
+//     console.log('Your Science score is ' + science);
+// }
+// showInfo(student);
 
-// -----------------------------3---------------------------
-// С помощью деструктуризации создайте новый массив (используйте rest и spread операторы)
+// // -----------------------------3---------------------------
+// // С помощью деструктуризации создайте новый массив (используйте rest и spread операторы)
 
-// до
-let rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-// ваш код ...
-let [first, second, ...rest] = rainbow
-rainbow = [rest , first, second];
-// после
-console.log(rainbow);
+// // до
+// let rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// // ваш код ...
+// let [first, second, ...rest] = rainbow
+// rainbow = [rest , first, second];
+// // после
+// console.log(rainbow);
