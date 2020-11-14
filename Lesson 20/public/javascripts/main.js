33f25ebe4dc54f96cd08e88afbf85a4ad9afe753
WebProjectGroup01 <WebProjectGroup@i.ua>
2020-11-15 00:49:18 +0200

    Server

--- Lesson 20/public/javascripts/main.js
+++ Lesson 20/public/javascripts/main.js
@@ -1,6 +1,9 @@
-
 class User {
-    constructor({name, email, password}) {
+    constructor({
+        name,
+        email,
+        password
+    }) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -26,19 +29,65 @@ class UserApi {
     }
 
 
+    static deleteUser(usersId) {
+        return fetch(UserApi.baseUrl, {
+            method: 'delete',
+            body: JSON.stringify({
+                usersId: usersId
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+        })
+    }
 
+    static updateUser(usersId, user) {
+        return fetch(UserApi.baseUrl, {
+            method: "put",
+            body: JSON.stringify({
+                ...user,
+                usersId
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+        })
+    }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    let form = document.querySelector('#regForm');
+    let formReg = document.querySelector('#regForm');
     let usersContainer = document.querySelector('#users');
-    // console.dir(form.elements);
 
-    form.addEventListener('submit', (e) => {
+    let formUbd = document.querySelector('#updForm');
+    let elemOfForm = formUbd.elements;
+
+    formUbd.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let user = new User({
+            name: elemOfForm.name.value,
+            email: elemOfForm.email.value,
+            password: elemOfForm.password.value,
+        });
+        let usersId = elemOfForm.idUbd.value;
+
+        UserApi.updateUser(usersId, user).then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+        });
+        formUbd.reset();
+    })
+
+
+    formReg.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        let { name, email, password } = form.elements;
+        let {
+            name,
+            email,
+            password
+        } = formReg.elements;
         let user = new User({
             name: name.value,
             email: email.value,
@@ -47,56 +96,50 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(user);
 
         UserApi.sendUser(user)
-                .then( response => {
-                    console.log(response);
-                    form.style.display = "none";
-                    usersContainer.style.display = "block";
-                })
+            .then(response => {
+                console.log(response);
+                formReg.style.display = "none";
+                usersContainer.style.display = "block";
+            })
     })
 
 
 
     let controls = document.querySelector('#controls');
     controls.addEventListener('click', (e) => {
-        if(e.target.id == "add") {
+        if (e.target.id == "add") {
+            formReg.style.display = "block";
+            usersContainer.style.display = "none";
 
         }
-        if(e.target.id == "ubdate") {
+        if (e.target.id == "get") {
             usersContainer.style.display = "block";
-            form.style.display = "none";
-            
+            formReg.style.display = "none";
+
             renderUserList()
         }
     })
 
-    let controls = document.querySelector('#delAndUbdate');
-    controls.addEventListener('click', (e) => {
-        if(e.target.id == "del") {
-            form.style.display = "block";
-            usersContainer.style.display = "none";
+    let delbtn = document.querySelector('#delbtn');
+    delbtn.addEventListener('click', (e) => {
+        let inputDel = document.querySelector("#inputDel");
+        let id = inputDel.value;
+            UserApi.deleteUser(id);
 
-        }
-        if(e.target.id == "get") {
-            usersContainer.style.display = "block";
-            form.style.display = "none";
-            
-            renderUserList()
-        }
     })
 
     function renderUserList() {
         UserApi.getUsers()
-                .then( res => res.json())
-                .then( data => data.data)
-                .then( users => {
-                    usersContainer.innerHTML = '';
-                    users.forEach( user => {
-                        usersContainer.innerHTML += `
+            .then(res => res.json())
+            .then(data => data.data)
+            .then(users => {
+                usersContainer.innerHTML = '';
+                users.forEach(user => {
+                    usersContainer.innerHTML += `
                             <h1 class="name">${user.name}</h1>
                             <p class="email">${user.email}</p>
                         `
-                    })
                 })
+            })
     }
 })
-
--- Lesson 20/server/routes/users.js
+++ Lesson 20/server/routes/users.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const db = require('../database/database');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let users = db.get('users');
   res.status(200).json({
     status: 'succes',
     data: users,
-  }); 
+  });
 
-}).post('/', function(req, res, next) {
+}).post('/', function (req, res, next) {
   console.log(req.body);
   if (!req.body) {
     return res.status(400).json({
@@ -21,19 +21,39 @@ router.get('/', function(req, res, next) {
   }
 
   let usersId = shortid.generate();
-  db.get('users').push({...req.body, usersId }).write();
+  db.get('users').push({
+    ...req.body,
+    usersId
+  }).write();
 
   res.status(200).json({
     status: 'succes',
     data: usersId,
   });
 
-}).put('/:id', function(req, res, next, ID, newUser) {
-  db.get('users').find({ id: ID }).assign(newUser).write();
+}).delete('/', function(req, res, next) {
+  let id = req.body;
+ 
+  id = id.usersId;
 
-}).delete('/:id', function(req, res, next, ID) {
-  let idUser = req.id;
-  db.get('users').remove({ id: idUser}).write();
-});
+  db.get('users').remove((item) => item.usersId == id).write();
+  
+  res.status(200).json({
+    status: 'success',
+    data: id,
+  })
+}).put('/', function (req, res, next) {
+  let user = req.body;
+
+  db.get('users').find({
+    usersId: user.usersId
+  }).assign(user).write();
+
+  res.status(200).json({
+    status: 'success',
+    data: user,
+  })
+})
+
 
 module.exports = router;
